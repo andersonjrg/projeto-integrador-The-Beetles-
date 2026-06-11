5cc0025ed7a8db9ab78e65da2ec80a41786b8353
MatheusCristophi <matheuscr909@gmail.com>
2026-06-10 22:24:47 -0300

    corrigindo espaço que apareceu sozinho

--- src/main/java/com/Beetles/systempayout/SystemPayoutApplication.java
+++ src/main/java/com/Beetles/systempayout/SystemPayoutApplication.java
@@ -1,6 +1,6 @@
 package com.Beetles.systempayout;
 
-import git .springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
